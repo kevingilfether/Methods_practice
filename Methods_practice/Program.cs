b967a7586a240e8f409a3f648ea8113f3cfbdd0e
kevingilfether <kevingilfether@gmail.com>
2017-10-04 14:24:04 -0400

    Made 'add' method

--- Methods_practice/Program.cs
+++ Methods_practice/Program.cs
@@ -10,6 +10,20 @@ namespace Methods_practice
     {
         static void Main(string[] args)
         {
+            
         }
+        //Methods are reuasable bits of code 
+        //Methods do one thing and do them really well
+        //Methods get written out here, outside of main, 
+        //but on the same level (and typically after)
+
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+
+            return sum;
+        }
+
+
     }
 }
